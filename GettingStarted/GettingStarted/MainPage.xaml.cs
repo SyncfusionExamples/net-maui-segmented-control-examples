9f31862b738fe4f5bfb5dab628b97cf93a6e712a
Kerubananthan-Ganesan <104969840+Kerubananthan3761@users.noreply.github.com>
2023-09-14 10:08:33 +0530

    Added getting started sample

---
+++ GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.Maui.Buttons;
+
+namespace GettingStarted;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        SfSegmentedControl segmentedControl = new SfSegmentedControl();
+        List<SfSegmentItem> itemList = new List<SfSegmentItem>
+        {
+            new SfSegmentItem() {Text = "Day"},
+            new SfSegmentItem() {Text = "Week"},
+            new SfSegmentItem() {Text = "Month"},
+            new SfSegmentItem() {Text = "Year"},
+        };
+        segmentedControl.ItemsSource = itemList;
+        this.Content = segmentedControl;
+    }
+
+}
+
