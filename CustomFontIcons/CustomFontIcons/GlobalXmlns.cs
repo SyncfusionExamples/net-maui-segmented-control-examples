da613f0692e8af0ad719e9ecaa9b17df23ba87ae
Vidyalakshmi Mani <vidyalakshmi.mani@syncfusion.com>
2025-08-22 13:06:45 +0530

    Added the sample for using custom font file to display font icons in the the segment items

---
+++ CustomFontIcons/CustomFontIcons/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CustomFontIcons")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CustomFontIcons.Pages")]
